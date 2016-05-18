22462a377a2100c1d90a044ca69329a1cd6a4404
twl8n <twl8n@virginia.edu>
2016-05-17 21:46:18 -0400

    Converted check to loop recur

--- problem_7.clj
+++ problem_7.clj
@@ -11,10 +11,11 @@
 ;; We know about 2 as the first prime, so initialize our list of found primes with 2
 
 (def foundp [2])
-(def wanted 10001)
+(def wanted 100)
 
-(defn checkp [possib]
+(defn checkp [possib fpcheck]
   (do
+      (prn 'possib possib 'fpcheck fpcheck)
       ;; Exclude 0, 1, and any prime we've already found
       (if (or 
            (= 0 possib)
@@ -22,44 +23,35 @@
            (some #(= possib %) foundp))
           (do
               ());; implicit return
+        (if (= 1
+               (loop [fpc (first fpcheck)
+                     tail (rest fpcheck)]
+                     ;; (def var (first fpcheck))
+                     ;; (def fpcheck (rest fpcheck))
+                     (prn 'checking fpc)
+                     (if (nil? fpc)
+                         1
+                       (do
+                           (if (and 
+                                (not= fpc possib)
+                                (= 0 (mod possib fpc)))
+                               0)
+                           (recur (first tail) (rest tail))))))
         (do
-            ;; Check factors against fpc, which gets consumed in the process.
-            (def fpc foundp)
-            ;; Using a globals as a loop controller and state var is less than ideal.
-            (def go 1)
-          (def is_prime 1)
-          (while (and (= go 1) (not (empty? fpc)))
-            (do
-                (def var (first fpc))
-                (def fpc (rest fpc))
-              (if (and 
-                   (not= var possib)
-                   (= 0 (mod possib var)))
-                  (do
-                      (def go 0)
-                    (def is_prime 0)
-                    ))))
-          (if (= is_prime 1)
-              (do
-                  ;; Add the prime we found to the global foundp list.
-                  (def foundp (conj foundp possib))))
-        ))))
+            ;; Add the prime we found to the global foundp list.
+            (def foundp (conj foundp possib))))
+    )))
   
 (defn -main []
-  (prn 'Lookin 'for 'the wanted 'th 'prime)
-  ;; Use icky vars rather than figuring out how to bind locally with let
-  (with-local-vars 
-   ;; We already have 2 as the first, so begin looping with 3
-   [checkme 3]
-   (while (< (count foundp) wanted)
-     (checkp (var-get checkme))
-     (if (= 0 (mod (var-get checkme) 5000))
-         (prn 'Working 'on (var-get checkme) 'Found (count foundp) 'primes))
-     (do
-         (var-set checkme (+ 1 (var-get checkme)))
-       )))
-  (prn (nth foundp (- wanted 1)) 'is 'the wanted 'prime)
-  )
+  (prn 'Looking 'for 'the wanted 'th 'prime)
+  ;; We already have 2 as the first, so begin looping with 3
+  (loop [checkme 3]
+        (when (< (count foundp) wanted)
+          (checkp checkme foundp)
+          (if (= 0 (mod checkme 50))
+              (prn 'Working 'on checkme 'Found (count foundp) 'primes))
+          (recur (+ checkme 1))))
+  (prn (nth foundp (- wanted 1)) 'is 'the wanted 'prime))
 
 
 
