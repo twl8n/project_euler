9701ad890969c0b735c00d553a3febc48ff58aa7
twl8n <tom@laudeman.com>
2016-06-28 22:03:26 -0400

    Add some new functions, esp. for sorting and aggregation. Clean up some thinking out loud

--- deftish_1.clj
+++ deftish_1.clj
@@ -4,26 +4,22 @@
 
 ;; todo:
 
-;; - + Now that we have a working col-max that takes a table and returns a map of column keys and max column
+;; - Implement Deft dcc (declare control column)
+
+;; - Seems like a good idea to have a where-true-for-all-rows since we have used that at least twice.
+
+;; - x Add a column arg to rowid, sort entire table based on come criteria, then call rowid with new column
+;;     arg to add a column that is numerically sorted based on the table row sort order. This assumes
+;;     operation order and (essentially) a single transactions for the sort and rowid.
+
+;; - x Now that we have a working col-max that takes a table and returns a map of column keys and max column
 ;;     string length, add this to (dpp) in the (map) that creates the per-column format string.
-
-;; - + create an aggregation function, adapting exising clojure, and figure out where aggregation values
-;;   go. (They go into all rows?)
-
-;; - (add-column) is single-value on and can't deal with multivalued columns. We need a multi-value
-;;   (add-column).
-
-;; Not working: (add-column 'maxlen-make (fn [arg %1] (prn %1)))
-
-;; Working: 3 variants of with-accum that will set multi-value column with (reductions) or set a single value
-;; without the (reductions)
 
 ;; - add some tests
 
 ;; - Improve (dpp) Add a list of cols to print, cols with max length of each col's values, use a function like
 ;;   one of with-accum functions to dynamically cal the col widths.  Also, might be nice to calculate all col
 ;;   widths in a single pass. That would be like a Deft aggration code block.
-
 
 ;; The Def table is a set of maps. Each map has the same keys. Rows of the table are the maps, and columns are
 ;; the keys. Note that every map has the same keys, which is critical since the keys are columns, or if you
@@ -34,9 +30,6 @@
 ;; lein repl
 ;; (load-file "deftish_1.clj")
 ;; (deftish_1/demo-deft)
-
-;; user=> (deftish_1/demo-deft)
-;; #{{:make volkswagen, :model golf, :disp nil, :color white} {:make "Toyota", :model "corolla", :disp 1.8, :color white} {:make Mercedes-Benz, :model "220b", :disp nil, :color white}}
 
 
 ;; When adding a new row, need to start with an empty row {:key nil} for all keys.
@@ -67,19 +60,6 @@
   #{{:make "toyota" :model "corolla"}
   {:make "volkswagen" :model "golf"}})
 
-;; Need a map of col names and max value width for dpp.
-
-;; deftish_1=>  (map #(str (name %)) (keys (first (demo-table))))
-;; ("make" "model")
-
-;; deftish_1=>  (keys (first (demo-table)))
-;; (:make :model)
-
-;; deftish_1=> (reduce (fn foo [map key](merge map {key 0})) {} (keys (first (demo-table))))
-;; {:make 0, :model 0}
-
-;; deftish_1=> (reduce-kv (fn foo [map key val](merge map {key 0})) {} (first (demo-table)))
-;; {:make 0, :model 0}
 
 ;; Aggregation: retains states between rows. Return max string length of values in column col
 ;;
@@ -140,8 +120,8 @@ key which might be longer than any of the data values."
 ;; Might be able to call add-column to store the result.
 
 
-;; (map) wants to return a list, so we have to (set) that into a set.  Also shows how to send a var with a
-;; list of args to (assoc) via (apply (partial )) as opposed to passing hard coded arguments.
+;; (map) returns a list, so we have to (set) that into a set.  This shows how to send a var containing a list
+;; of args to (assoc) via (apply (partial )) as opposed to passing hard coded arguments.
 
 (defn where
   "Like a where clause run against each row of table. If the constraint is true do the rowfun on this row."
@@ -150,135 +130,56 @@ key which might be longer than any of the data values."
                  (apply (partial assoc %) (rowfun %))
                %) table)))
 
-;; Need to use map to get a different value for each row, but reduce to aggregate across rows. Agg functions
-;; like dcc and desc can have multi-valued results. dcc is special because it makes a sortable column in a
-;; single pass. The code below won't do anything like that, so it needs work.
-;; 
-;; Might need an initializer for zero
-;; (constraint) needs to return true-val false-val just like (if)
-;; or return true value and the else simply returns %1 which is result.
+(declare demo-deft)
 
-;; This adds a column with an accumulator, essentially an aggregation operation using (reductions) which
-;; creates a lazy sequence.
+;; Add a rowid. Numbering starts with 1. Rows numbers reflect the sort order of the table rows at the time
+;; rowid is called. We can use this to do a sql-like unique row access. We could map a sequence against the
+;; table, even if the sequence is an out of order map using the rowid values as keys. This could be used for
+;; sort. The alternative is Noah's Church-encoding sortable keys, as used in Deft dcc. Implementing dcc might
+;; be a good idea, even if rowid as a key works.
 
-(defn with-accum1 [table]
-  (map #(assoc %1 :accum %2) table
-       (reductions str (map :model table))))
+;; Rows are numbered descending so they'll be in the opposite order of the alphabetic sorting of :model. I
+;; need something sorted reversed for testing, and the rows seem to stay ordered, and I haven't yet figured
+;; out why.
 
-;; Using a reductions fn that always returns 1 we get a simple add-by-one.
-
-(defn with-accum2 [table]
-  (map #(assoc %1 :accum %2) table
-       (reductions + (map (fn [row] 1) table))))
-
-;; Leaving off the reductions, we simply get a single valued new column.
-;; This is an alternate implementation of add-column.
-
-(defn with-accum3 [table]
-  (map #(assoc %1 :accum %2) table
-       (map (fn [row] 1) table)))
+(defn rowid [table col]
+  (let [newcol (keyword col)]
+  (map #(assoc %1  newcol %2) table
+       (reductions + (map (fn [row] 1) table)))))
 
 
-(defn agg-where [table constraint func]
-  (let [multival 
-    (reduce 
-     #(let [row %2
-           result %1]
-           (if (constraint row result)
-               (func row result)
-             result))  0 table)]
-             (where table (fn [row] (true))
-                    (fn [row] [:maxlen-model multival]))))
+;; This is Deftish clojure, using the thread first macro (->). This demos add-row, add-column and the exciting
+;; (where) function which takes two fn's as args. Also the less elegant function (merc-220) which works, but
+;; relies knowledge of deftish internals.
 
+;; The idioms for aggregation are not fully developed. There are two: 1) aggregate all rows for a single value
+;; per column. 2) aggregate all rows and assign each row its own value. These are explored in some of the
+;; functions necessary for Deft pretty print (dpp) above.
 
-;; This is Deftish clojure, using the thread first macro (->).
-;; This demos the (where) function which takes two fn's as args.
-;; Also less elegant function (merc-220) which works, but uses knowledge of deftish internals.
+;; Discussion: Pure Deft would use "variable" names and not keys, as well as more natural syntax in
+;; general. Forcing the user to fuss with keys, and to wrap all their code inside a macro makes this Deft-ish
+;; and not real Deft. At the same time, Lisp syntax makes the Deft/Deft-ish distinction less distinct.
+
 (defn demo-deft []
-  (-> #{} (add-row {:make "toyota" :model "corolla"})
+  (-> #{}
+      (add-row {:make "mercedes" :model ""})
+      (merc-220)
       (add-column 'disp 0)
-      (add-row {:make "mercedes"})
       (add-row {:make "volkswagen" :model "golf"})
-      (add-column 'color 'white)
+      (add-row {:make "toyota" :model "corolla"})
+      (add-column "color" "white")
       (where (fn [row] (= "toyota" (:make row)))
              (fn [row] [:make "Toyota" :disp 1.8]))
       (where (fn [row] (and (= "golf" (:model row)) (= "volkswagen" (:make row))))
              (fn [row] [:disp 2.2]))
-      (merc-220)
-      ((fn [table] (add-column table :maxlen-make (agg-maxlen table 'make))))))
+      (where (fn [row] (= "220b" (:model row)))
+             (fn [row] [:disp 2.2 :color "green"]))
+      ((fn [table] (add-column table :maxlen-make (agg-maxlen table 'make))))
+      (where (fn [row] true)
+             (fn [row] [:model (string/capitalize (:model row)) :make (string/capitalize (:make row))]))
+      ((fn [table] (sort-by :make table)))
+      (rowid 'make-sort)
+      (where (fn [row] true)
+             (fn [row] [:cid (* 61.0128 (:disp row))]))))
 
-
-      ;; (agg-where (fn [row result] (> result (count (str (:model row)))))
-      ;;            (fn [row result] [:maxlen-model (count (str (:model row)))]))))
-
-;; Everything below is historical, or notes and will be removed soo.
-  
-;; Nest function calls to build and modify a table. -> thread-first macro makes this way more legible.
-(defn -main []
-  (let [table #{}]
-       (merc-220
-       (add-column
-        (add-row 
-         (add-row 
-          (add-column 
-           (add-row table {:make "toyota" :model "corolla"})
-           'disp 0)
-          {:make "mercedes"})
-         {:make 'volkswagen :model 'golf})
-        'color 'white))))
-
-
-;; Assume that the first row has all columns. This is normally a safe assumption.  A set of table keys is not
-;; what was needed for add-row. Maybe it will be useful for something else.
-(defn table-keys [table]
-  (set (vec (keys (first table)))))
-
-(defn t1 []
-  (table-keys #{{:make 'toyota :model 'corolla}
-              {:make 'volkswagen :model 'golf}}))
-
-(defn t2 [table]
-  (merge (empty-row table) {:make "mercedes"}))
-
-;; reduce will use 2 args, and if supplied with 1 arg it will eat the first 2 components of arg 1
-;; Supply reduce with 2 args: set set
-;; This fails if arg 1 is a map, no surprise since we aren't supplying kv pairs.
-;; Works if arg 1 is [] () or #{}
-(defn add-column-demo-2 []
-  (reduce #(conj %1 (assoc %2 :disp 0)) 
-          #{} 
-          #{{:make 'toyota :model 'corolla} {:make 'volkswagen :model 'golf}}))
-
-
-(defn update-column-demo []
-  (reduce #(conj %1
-               (update-in %2 [:make] clojure.string/capitalize))
-               #{}
-               #{{:make 'toyota :model 'corolla} {:make 'volkswagen :model 'golf}}))
-
-
-;; Works, sort of. Adds new column :disp for existing :make, but fails to add new row :make and the failure is
-;; silent. Need to look at the source for (join) and figure out how to get it to detect and/or report
-;; non-joinable data. Or need some way to stop users from asking for things they shouldn't ask for.
-(defn add-column-demo-1 []
-  (let [result (conj ()
-                     {:make 'toyota :model 'corolla}
-                     {:make 'volkswagen :model 'golf}
-                     )
-       new (conj ()
-                 {:make 'volkswagen :disp 1.8}
-                 {:make 'toyota :disp 1.0}
-                 {:make 'mercedes :disp 2.2}
-                 )]
-                   (clojure.set/join result new)))
-
-;; Works. Select a single column of all rows, returns a set.
-;; Using (conj () ...) this returns a list instead of a set.
-(defn single-col-map []
-  (let [result #{
-       {:make 'toyota :model 'corolla}
-       {:make 'volkswagen :model 'golf}
-       }]
-       (reduce (fn [set row] 
-                   (conj set (select-keys row [:make] ))) #{} result)))
 
