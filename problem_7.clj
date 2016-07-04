7c0b2b81294a140e303fc0da403ec63f788c9d6e
twl8n <tom@laudeman.com>
2016-07-04 17:45:04 -0400

    Remove debugs, improve functional idioms, add a second solution using reduce-reduced. Update comments.

--- problem_7.clj
+++ problem_7.clj
@@ -3,52 +3,60 @@
 
 ;; https://projecteuler.net/problem=7
 
-;; java -cp ~/Library/Clojure/clojure-1.8.0.jar:. clojure.main --main problem_7
-
 ;; By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.  What
 ;; is the 10001st prime number?
 
 ;; 541 is the 100 th prime
 ;; 104743 is the 10001 th prime
 
-;; We know about 2 as the first prime, so initialize our list of found primes with 2
+;; We know about 2 as the first prime, so initialize our sequence of found primes with the sequence [2]
 
-(def wanted 10001)
+;; lein repl
+;; (load-file "problem_7.clj")
+;; (problem_7/solution)
+;;=> {:wanted 100, :prime 541}
+;; (problem_7/solution 100)
+;;=> {:wanted 100, :prime 541}
+;; (problem_7/solution 10001)
+;;=> {:wanted 10001, :prime 104743}
 
-;; Return possib if it is prime, else return nil
+;; Return tru if possib is prime, else false.
 (defn checkp [possib fpcheck]
   (loop [fpc (first fpcheck)
         fprest (rest fpcheck)]
-        (cond (nil? fpc) possib ; Return a found prime
-              (and (not= fpc possib) (= 0 (mod possib fpc))) nil ; Not a prime, return nil
-              true (recur (first fprest) (rest fprest))) ; Keep working
-        ))
+        (cond (nil? fpc) true ; Found a prime
+              (and (not= fpc possib) (= 0 (mod possib fpc))) false ; Not a prime
+              true (recur (first fprest) (rest fprest)))))
 
-(defn -main []
-  (prn 'Looking 'for 'the wanted 'th 'prime)
-  ;; We already have 2 as the first prime, so begin looping with 3
-  (let [final
-    (loop [checkme 3
-          foundp [2]]
-          (if (>= (count foundp) wanted)
-              (nth foundp (- wanted 1)) ;; Done, final prime is the returned value of the loop
-            (do ;; else check candidate checkme inside the recurs
-                ;; Print a status message every few checks
-                (if (= 0 (mod checkme 2000))
-                    (prn 'Working 'on checkme 'Found (count foundp) 'primes))
-                (recur (+ checkme 1)
-                       ;; If we get back our checkme, it must be prime so conj it to the foundp seq which will
-                       ;; be used in the next recur through the loop, else use foundp is unchanged.
-                       (if (= checkme (checkp checkme foundp))
-                           (conj foundp checkme)
-                         foundp)
-                       )
-              )
-            )
-          )]
-           (str final 'is 'the wanted 'th 'prime)
-           )
-  )
+;; loop-recur is somewhat the more obvious way to loop until we have the wanted prime.
+(defn solution [ & wanted]
+  (let [ wanted (or (first wanted) 100)]
+       (loop [checkme 3
+             foundp [2]
+             cf (count foundp)]
+             (if (>= cf wanted)
+                 {:wanted cf :prime (last foundp)}
+                 (recur (+ checkme 1) 
+                        (if (checkp checkme foundp)
+                            (conj foundp checkme)
+                          foundp) 
+                        (count foundp))))))
 
+;; A second solution using reduce-reduced and lazy sequence may be a bit faster, and more functional
+;; lein repl
+;; (load-file "problem_7.clj")
+;; (problem_7/s2)
+;;=> {:wanted 100, :prime 541}
+
+(defn s2 [& wanted]
+  (let [ wanted (or (first wanted) 100)]
+       (reduce 
+        (fn [foundp checkme]
+            (let [cf (count foundp)]
+                 (if-not (>= cf wanted)
+                         (if (checkp checkme foundp)
+                             (conj foundp checkme)
+                           foundp)
+                         (reduced {:wanted cf :prime (last foundp)})))) [2] (iterate inc 3))))
 
 
